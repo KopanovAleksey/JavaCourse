98e50a7264432c306fab857c70cfecbaab618e26
Alexey <asaleseksesey12345@gmail.com>
2023-06-23 22:30:17 +0300

    New repos created

---
+++ seminars/src/main/java/ru/Kopanov/api/Homework1/Homework1.java
@@ -0,0 +1,7 @@
+package ru.Kopanov.api.Homework1;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ seminars/src/main/java/ru/Kopanov/api/Seminar1/Lesson1.java
@@ -0,0 +1,33 @@
+package ru.Kopanov.api.Seminar1;
+
+import java.util.Scanner;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        task2();
+    }
+
+    static void printName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите ваше имя: ");
+        String name = scanner.nextLine();
+        System.out.printf("Привет, %s !", name);
+        scanner.close();
+    }
+
+    static void task2() {
+        int counter = 0;
+        int max = 0;
+        int[] array = { 1, 1, 0, 1, 1, 1, 0, 1 };
+        for (int i : array) {
+            if (i == 1)
+                counter++;
+            else{
+                if (counter > max)
+                    max = counter;
+                    counter = 0;
+            }
+        }
+    System.out.println(max);
+    }
+}
